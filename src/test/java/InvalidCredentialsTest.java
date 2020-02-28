b67c50ea58d2b8df07687c1e595b7d43e18a4c48
MikhailNakko <Mykhailo_nakko@epam.com>
2020-02-28 12:43:58 +0200

    Pom modified

--- src/test/java/InvalidCredentialsTest.java
+++ src/test/java/InvalidCredentialsTest.java
@@ -2,9 +2,16 @@ import PageObjects.BasePage;
 import PageObjects.FaceBookSignUpAndLoginPage;
 import PageObjects.GoogleSearchPage;
 import PageObjects.GoogleSearchResultPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InvalidCredentialsTest extends BaseTest {
 
@@ -21,5 +28,6 @@ public class InvalidCredentialsTest extends BaseTest {
         Assert.assertEquals("The email or phone number you’ve entered doesn’t match any account. " +
                 "Sign up for an account.", alertMessageText);
     }
+
 
 }
